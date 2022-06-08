807b1eeadb7e2c216a052cad644a7d6b0b951b75
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-08 12:15:23 +0530

    changes in arc/App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Nav from './nav';
+import About from './about';
+import Shop from './shop';
+import { BrowserRouter as Router,  Route,Routes } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Nav/>
+    <Routes>
+    <Route path="/" exact element={<Home/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/shop" element={<Shop/>}/>
+    </Routes>
     </div>
+    </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+)
 
 export default App;
---
+++ src/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function About() {
+  return (
+    <div className="second">
+      <h1>About Page</h1>
+    </div>
+  );
+}
+
+export default About;
