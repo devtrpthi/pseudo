7ccfeaa70e837f2db79f5d71a7a68125c343df29
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-15 13:51:35 +0530

    App.js modified

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-import React from 'react';
-import './App.css';
-import Nav from './nav';
-import About from './about';
-import Shop from './shop';
-import { BrowserRouter as Router,  Route,Routes } from 'react-router-dom';
 
-function App() {
+import './App.css';
+
+import React, { useState } from 'react';
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
   return (
-    <Router>
-    <div className="App">
-    <Nav/>
-    <Routes>
-    <Route path="/" exact element={<Home/>}/>
-    <Route path="/about" element={<About/>}/>
-    <Route path="/shop" element={<Shop/>}/>
-    </Routes>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
-    </Router>
   );
 }
-
-const Home = () => (
-  <div>
-    <h1>Home Page</h1>
-  </div>
-)
-
-export default App;
