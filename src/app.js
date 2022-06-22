01a4245fa2a3c05347a20400dd322682f32c4481
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-22 16:55:02 +0530

    handleClick added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 import {React, useState} from "react";
 import  ReactDOM  from "react";
 
+function MyButton() {
+    const[count, setCount] = useState(1);
 
+    function handleClick(){
+        setCount(count + 1);
+    }
+}
   export default App;
