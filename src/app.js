f6c018353fe0a29d518b7adb0dffc02853985a2d
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-22 17:00:14 +0530

    counters added in app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ function MyButton() {
     function handleClick(){
         setCount(count + 1);
     }
+    return(
+        <button onClick={handleClick}>
+            Clicked {count} times
+        </button>
+    );
 }
+
+    function App() {
+        return(
+            <div>
+                <h1>Random counters</h1>
+                <MyButton/>
+                <MyButton/>
+            </div>
+        );
+    }
   export default App;
