f30d94e25360dd76d8c97d2723799a2b68477aaf
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-23 12:36:23 +0530

    index.js modified

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App from './app';
+import Test from '../components/test';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Test />
   </React.StrictMode>
 );
 
