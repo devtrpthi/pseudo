884696e2cd0188484e89bc937f8397faee8af37f
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-24 12:22:49 +0530

    src/index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Gallery from './app';
+import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Gallery />
+    <App />
   </React.StrictMode>
 );
 
