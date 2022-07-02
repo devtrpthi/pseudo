f615ff16a9a1cc4c05facd364cc3d67c527f8d49
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-02 19:58:57 +0530

    initial setup

--- fnd-master-learn/app.js
+++ fnd-master-learn/app.js
@@ -1,10 +1,9 @@
-
-        const App = () => {
-            return React.createElement(
-                "div",
-                {},
-                React.createElement("h1",{id: 'my-brand'}, "Adopt Me!")
+ const App = () => {
+    return React.createElement(
+            "div",
+            {},
+            React.createElement("h1",{id: 'my-brand'}, "Adopt Me!")
             );
         }
-        ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
     
