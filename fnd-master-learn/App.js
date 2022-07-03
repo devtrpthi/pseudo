895d94538d5be8c644fa804ce6b9d469d2e975eb
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-03 16:34:16 +0530

    pre-ship

--- fnd-master-learn/App.js
+++ fnd-master-learn/App.js
@@ -1,8 +1,8 @@
 const Pet = (props) => {
     return React.createElement("div",{}, [
-        React.createElement("h2",{},"Luna"),
-        React.createElement("h3",{},"Shadow"),
-        React.createElement("h3",{},"Milo"),
+        React.createElement("h2",{},props.name),
+        React.createElement("h3",{},props.animal),
+        React.createElement("h3",{},props.breed),
     ])
 }
 
