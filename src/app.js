6f9ff64223e3a351891bd3d30011540a79f5f75c
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-03 21:58:29 +0530

    modified

--- src/app.js
+++ src/app.js
@@ -4,17 +4,30 @@ import './App.css';
 import Pet from "./pet";
 
 
+//const App = () => {
+  //return React.createElement(
+    //      "div",
+      //    {},
+  //[        
+    //  React.createElement("h1",{id: 'my-brand'}, "Adopt Me!"),
+      //React.createElement(Pet, {name: "Luna", animal: "Dog", breed: "Havaneese"}),
+      //React.createElement(Pet,{name: 'Pepper', animal: 'bird', breed: 'Cackatiel'}),
+      //React.createElement(Pet,{name: 'Sudo', animal: 'Dog', breed: 'Wheaten Terrier'}),
+  //]    
+    //      );
+      //}
+
 const App = () => {
-  return React.createElement(
-          "div",
-          {},
-  [        
-      React.createElement("h1",{id: 'my-brand'}, "Adopt Me!"),
-      React.createElement(Pet, {name: "Luna", animal: "Dog", breed: "Havaneese"}),
-      React.createElement(Pet,{name: 'Pepper', animal: 'bird', breed: 'Cackatiel'}),
-      React.createElement(Pet,{name: 'Sudo', animal: 'Dog', breed: 'Wheaten Terrier'}),
-  ]    
-          );
-      }
+  return(
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name='Luna' animal='Dog' breed='Havaneese' />
+      <Pet name='Pepper' animal='bird' breed='Cackatiel'/>
+      <Pet name= 'Sudo' animal='Dog' breed= 'Wheaten Terrier'/>
+    </div>
+  );
+}
+
+
 export default App;
   
