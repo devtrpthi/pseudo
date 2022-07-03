c193a35a1c0bd8df01517ca0062ef37b2050445d
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-03 19:41:52 +0530

    added

--- src/app.js
+++ src/app.js
@@ -1,27 +1,26 @@
 import React from "react";
+import './App.css';
 
-
-import { people } from './data.js';
-import { getImageUrl } from './utils.js';
-
-export default function App() {
-  const listItems = people.map(person =>
-    <li key={person.id}>
-      <img
-        src={getImageUrl(person)}
-        alt={person.name}
-      />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  );
-  return (
-    <article>
-      <h1>Scientists</h1>
-      <ul>{listItems}</ul>
-    </article>
-  );
+const Pet = (props) => {
+  return React.createElement("div",{}, [
+      React.createElement("h2",{},props.name),
+      React.createElement("h3",{},props.animal),
+      React.createElement("h3",{},props.breed),
+  ])
 }
+
+
+const App = () => {
+  return React.createElement(
+          "div",
+          {},
+  [        
+      React.createElement("h1",{id: 'my-brand'}, "Adopt Me!"),
+      React.createElement(Pet, {name: "Luna", animal: "Dog", breed: "Havaneese"}),
+      React.createElement(Pet,{name: 'Pepper', animal: 'bird', breed: 'Cackatiel'}),
+      React.createElement(Pet,{name: 'Sudo', animal: 'Dog', breed: 'Wheaten Terrier'}),
+  ]    
+          );
+      }
+export default App;
+  
