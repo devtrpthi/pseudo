cf0b8adec19997d560b7882de4a3ab4deaf08563
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-04 17:02:41 +0530

    breeds

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -7,6 +7,8 @@ const ANIMALS = ['bird', 'cat', 'dog', 'rabbit' , 'reptile'];
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
     const [animal, setAnimal] = useState('');
+    const [breed, setBreed] = useState('');
+    const breeds = [];
 
     return(
         <div className="search-params">
@@ -25,6 +27,21 @@ const SearchParams = () => {
                     {ANIMALS.map((animal) =>(
                         <option value={animal} key={animal}>
                             {animal}
+                        </option>
+                    ))} 
+                    </select>
+                </label>
+
+                <label htmlFor="breed">
+                    Breed 
+                    <select
+                        id="breed" value={breed} onChange={(e) => setBreed(e.target.value)}
+                        onBlur={(e) => setBreed(e.target.value)}
+                    >
+                     <option/>
+                    {breeds.map((breed) =>(
+                        <option value={breed} key={breed}>
+                            {breed}
                         </option>
                     ))} 
                     </select>
