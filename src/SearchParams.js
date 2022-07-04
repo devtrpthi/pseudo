cd59df6c7d6f5bb6d0aed2d44004e929d93e2f5a
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-04 16:20:18 +0530

    hooks-use

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -6,13 +6,27 @@ const ANIMALS = ['bird', 'cat', 'dog', 'rabbit' , 'rpetile'];
 
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
-    
+    const [animal, setAnimal] = useState('');
+
     return(
         <div className="search-params">
             <form>
                 <label htmlFor="location">
                     Location
                     <input id="location" onChange={e => setLocation(e.target.value)} value={location} placeholder="Location"/>
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select
+                        id="animal" value={animal} onChange={e => setAnimal(e.target.value)}
+                        onBlur={e => setAnimal(e.target.value)}
+                    />
+                    <option/>
+                    {ANIMALS.map((animal) =>(
+                        <option value={animal} key={animal}>
+                            {animal}
+                        </option>
+                    ))}
                 </label>
                 <button>
                     Submit
