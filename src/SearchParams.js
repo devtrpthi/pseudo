49a71de2b3b2f3ab3a2e80f72bea62be0c2f3741
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-04 11:00:14 +0530

    label added

---
+++ src/SearchParams.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SearchParams = () => {
+    return(
+        <div className="search-params">
+            <form>
+                <label htmlFor="location">
+                    Location
+                    <input/>
+                </label>
+            </form>
+
+        </div>
+
+    );
+}
+
+export default SearchParams;
