75288e1bd6af7cedcf68877a4769c9bd8f5a15fb
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-04 16:24:27 +0530

    hooks-use

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -13,7 +13,7 @@ const SearchParams = () => {
             <form>
                 <label htmlFor="location">
                     Location
-                    <input id="location" onChange={e => setLocation(e.target.value)} value={location} placeholder="Location"/>
+                    <input id="location" onChange={(e) => setLocation(e.target.value)} value={location} placeholder="Location"/>
                 </label>
                 <label htmlFor="animal">
                     Animal
