9afa6d6829ac9b640a7a720d6af4415a14b84fbc
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-04 11:27:22 +0530

    jsx

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 const SearchParams = () => {
+    const location= 'Seattle, WA';
     return(
         <div className="search-params">
             <form>
                 <label htmlFor="location">
                     Location
-                    <input/>
+                    <input id="location" value={location} placeholder="location"/>
                 </label>
+                <button>
+                    Submit
+                </button>
             </form>
 
         </div>
