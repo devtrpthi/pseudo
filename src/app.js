12694cb4f08d7cefa7cb0f15e5f3dcc037193de9
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-04 11:35:18 +0530

    trash-out

--- src/app.js
+++ src/app.js
@@ -1,28 +1,12 @@
 import React from "react";
 import './App.css';
-import Pet from "./pet";
-
-
-//const App = () => {
-  //return React.createElement(
-    //      "div",
-      //    {},
-  //[        
-    //  React.createElement("h1",{id: 'my-brand'}, "Adopt Me!"),
-      //React.createElement(Pet, {name: "Luna", animal: "Dog", breed: "Havaneese"}),
-      //React.createElement(Pet,{name: 'Pepper', animal: 'bird', breed: 'Cackatiel'}),
-      //React.createElement(Pet,{name: 'Sudo', animal: 'Dog', breed: 'Wheaten Terrier'}),
-  //]    
-    //      );
-      //}
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return(
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name='Luna' animal='Dog' breed='Havaneese' />
-      <Pet name='Pepper' animal='bird' breed='Cockatiel'/>
-      <Pet name= 'Sudo' animal='Dog' breed= 'Wheaten Terrier'/>
+      <SearchParams/>
     </div>
   );
 }
