d310648a95681665b60243235745c01e1d35f80a
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-07 19:24:04 +0530

    class

--- src/Details.js
+++ src/Details.js
@@ -13,9 +13,14 @@ class Details extends Component {
             `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
         );
         const json = await res.json();
-        this.setState({
+        this.setState(
+            Object.assign(
+            {
             loading: false,
-        })
+            },
+             json.pets[0]
+        )
+        );
     }
     render () {
         return (
@@ -26,4 +31,4 @@ class Details extends Component {
 
 
 
-export default Details;
+export default withRouter(Details);
