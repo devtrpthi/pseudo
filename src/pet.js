ae831894c6fc7f020e05ca5ba4188b8954da8bd7
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-07 12:18:35 +0530

    refactored

--- src/pet.js
+++ src/pet.js
@@ -1,20 +1,29 @@
 import React from "react";
 
- //const Pet = (props) => {
-   // return React.createElement("div",{}, [
-     //   React.createElement("h2",{},props.name),
-       // React.createElement("h3",{},props.animal),
-        //React.createElement("h3",{},props.breed),
-    //])
-  //}
 
-  const Pet = (props) => {
+  const Pet = ({
+    name,
+    animal,
+    breed,
+    images,
+    location,
+    id
+  }) => {
+
+      let hero ='http://pets-images.dev-apis.com/pets/none.jpg';
+      if(images.length) {
+        hero=images[0];
+      }
     return(
-        <div>
-        <h2>{props.name}</h2>
-        <h2>{props.animal}</h2>
-        <h2>{props.breed}</h2>
-        </div>
+        <a href={`/details/${id}`} className='pet'>
+          <div className="image-container">
+            <img src={hero} alt={name}/>
+          </div>
+          <div>
+            <h1>{name}</h1>
+            <h2>{`${animal} - ${breed}- ${location}`}</h2>
+          </div>
+        </a>
     );
   }
 
