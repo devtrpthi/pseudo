a73452a29ec1a9d6dbe2a1cc21d71ab1fc5dc0dc
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-07 12:45:30 +0530

    routes-added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 import React from "react";
+import  ReactDOM  from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return(
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams/>
+      <Router>
+      <Route path='/details/:id'>
+          <Details/>
+      </Route>
+        <Route path='/'>
+        <SearchParams/>
+        </Route>
+      </Router>
     </div>
   );
 }
