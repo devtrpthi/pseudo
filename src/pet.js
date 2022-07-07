fc7bb7b51cefe67416490d3217e2fc335ce8625d
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-07 12:23:43 +0530

    info added

--- src/pet.js
+++ src/pet.js
@@ -19,7 +19,7 @@ import React from "react";
           <div className="image-container">
             <img src={hero} alt={name}/>
           </div>
-          <div>
+          <div className="info">
             <h1>{name}</h1>
             <h2>{`${animal} - ${breed}- ${location}`}</h2>
           </div>
