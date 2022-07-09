903b541a5a62cbd42b1750c4f2a09f7906cedf3b
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-09 16:27:34 +0530

    modified

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -4,6 +4,18 @@ import { Link } from "react-router-dom";
 class ErrorBoundary extends Component{
     state = {hasError: false};
     static getDerivedFromError(){
-        return(hasError: true)
+        return{hasError: true}
+    }
+    componentDidCatch(error, info) {
+        console.error('ErrorBoundary caught an error', error, info);
+    }
+    render() {
+        if(this.state.hasError) {
+            return(
+                <h1>
+                    This listing has an error.<Link to='/'>Click Here</Link> to go back to the home page.
+                </h1>
+            )
+        }
     }
 }
